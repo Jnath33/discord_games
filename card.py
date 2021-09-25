f9061a86c19c6de27be102bb4e9bedc92cd844ea
Nathan Jaguenaud <nathan@kimsufi3.jaguenaud.org>
2021-09-25 18:08:15 +0200

    Add comparator

--- card.py
+++ card.py
@@ -1,16 +1,25 @@
 from enum import Enum
+from filecmp import cmp
 
 
 class Color(Enum):
-    COEUR = {"id": "co", "emoji": (":heart:", "❤️️")}
-    CARREAUX = {"id": "ca", "emoji": (":diamonds:", "♦️")}
-    TREFLE = {"id": "tr", "emoji": (":clubs:", "♣️")}
-    PIQUE = {"id": "pi", "emoji": (":spades:", "♠️")}
+    COEUR = {"id": "co", "emoji": (":heart:", "❤️️"), "int": 1}
+    CARREAUX = {"id": "ca", "emoji": (":diamonds:", "♦️"), "int": 2}
+    TREFLE = {"id": "tr", "emoji": (":clubs:", "♣️"), "int": 3}
+    PIQUE = {"id": "pi", "emoji": (":spades:", "♠️"), "int": 4}
 
 
 to_atout = {7: 1, 8: 2, 12: 3, 13: 4, 10: 5, 1: 6, 9: 7, 11: 8}
 to_normal = {7: 1, 8: 2, 9: 3, 11: 4, 12: 5, 13: 6, 10: 7, 1: 8}
 atout_color = None
+nomber_to_name = {7:"7",8:"8",9:"9",10:"10",11:"Valet",12:"Dame",13:"Roi",1:"As"}
+
+
+def to_buttons(card_list):
+    card_list.sort()
+    for card in card_list:
+        print(str(card))
+
 
 
 def set_atout_color(color):
@@ -18,7 +27,7 @@ def set_atout_color(color):
 
 
 class Card:
-    def __init__(self, color: Color, nomber: int, player: str):
+    def __init__(self, color: Color, nomber: int, player):
         self.color = color
         self.nomber = nomber
         self.player = player
@@ -37,6 +46,30 @@ class Card:
                     return False
         c_list = to_atout if self.color == atout_color else to_normal
         return c_list[self.nomber] > c_list[card.nomber]
+
+    def __cmp__(self, other):
+        return cmp((self.color.value["int"]*100 + self.nomber), (other.color.value["int"]*100+other.nomber))
+
+    def __eq__(self, other):
+        return (self.color.value["int"]*100 + self.nomber) == (other.color.value["int"]*100+other.nomber)
+
+    def __ne__(self, other):
+        return (self.color.value["int"]*100 + self.nomber) != (other.color.value["int"]*100+other.nomber)
+
+    def __lt__(self, other):
+        return (self.color.value["int"]*100 + self.nomber) < (other.color.value["int"]*100+other.nomber)
+
+    def __le__(self, other):
+        return (self.color.value["int"]*100 + self.nomber) <= (other.color.value["int"]*100+other.nomber)
+
+    def __gt__(self, other):
+        return (self.color.value["int"]*100 + self.nomber) > (other.color.value["int"]*100+other.nomber)
+
+    def __ge__(self, other):
+        return (self.color.value["int"]*100 + self.nomber) >= (other.color.value["int"]*100+other.nomber)
+
+    def __str__(self):
+        return self.color.value["id"]+" "+nomber_to_name[self.nomber]
 
 
 def beats(color_start, cards):
