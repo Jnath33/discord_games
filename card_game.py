0e249fe6fb93ffac2fc808f4c43a631bffad7799
Nathan Jaguenaud <nathan@kimsufi3.jaguenaud.org>
2021-09-26 08:08:38 +0200

    Add the select of atout

--- card_game.py
+++ card_game.py
@@ -11,6 +11,7 @@ pos_to_relative_pos = {"n": {"s": "n", "o": "e", "n": "s", "e": "o"},
                        "s": {"s": "s", "n": "n", "o": "o", "e": "e"},
                        "e": {"s": "o", "n": "e", "o": "n", "e": "s"},
                        "o": {"s": "e", "n": "o", "o": "s", "e": "n"}}
+get_next = {"n": "e", "e": "s", "s": "o", "o": "n"}
 
 
 class Game:
@@ -26,6 +27,22 @@ class Game:
         self.ctx = ctx
         self.p_to_inter = {}
         self.channels = {}
+        self.hand_card = {"n": [], "s": [], "o": [], "e": []}
+        t_cards = [[card.Card(i, m, None) for m in range(7, 14)] for i in [card.Color.COEUR,
+                                                                           card.Color.TREFLE,
+                                                                           card.Color.CARREAUX,
+                                                                           card.Color.PIQUE]]
+        self.cards = []
+        for i in t_cards:
+            self.cards += i
+        for i in [card.Color.COEUR,
+                  card.Color.TREFLE,
+                  card.Color.CARREAUX,
+                  card.Color.PIQUE]:
+            self.cards.append(card.Card(i, 1, None))
+
+        random.shuffle(self.cards)
+        self.distribue(5)
 
     async def end_init(self):
         guild = self.ctx.guild
@@ -40,6 +57,7 @@ class Game:
             del overwrites[self.players[it]]
 
     async def start(self):
+
         print(f"Game {self.id} is starting")
         msgs = {"n": await self.channels["n"].send(self.players["n"].mention),
                 "s": await self.channels["s"].send(self.players["s"].mention),
@@ -52,6 +70,49 @@ class Game:
                 style=ButtonStyle.green,
                 label="Oui",
                 custom_id="y"
+            )
+        )
+        yes_no_button = ActionRow(
+            Button(
+                style=ButtonStyle.green,
+                label="Oui",
+                custom_id="y"
+            ),
+            Button(
+                style=ButtonStyle.red,
+                label="Non",
+                custom_id="n"
+            )
+        )
+        deux_button = ActionRow(
+            Button(
+                style=ButtonStyle.gray,
+                label="",
+                custom_id="co",
+                emoji="❤️"
+            ),
+            Button(
+                style=ButtonStyle.gray,
+                label="",
+                custom_id="ca",
+                emoji="♦️"
+            ),
+            Button(
+                style=ButtonStyle.gray,
+                label="",
+                custom_id="tr",
+                emoji="♣️"
+            ),
+            Button(
+                style=ButtonStyle.gray,
+                label="",
+                custom_id="pi",
+                emoji="♠️"
+            ),
+            Button(
+                style=ButtonStyle.gray,
+                label="Deux",
+                custom_id="de"
             )
         )
         embed = discord.Embed(color=0xff8800)
@@ -77,13 +138,61 @@ class Game:
         embed = discord.Embed(color=0x37ff00)
         embed.set_footer(text="This game was made by Jnath#5924")
         embed.add_field(name="Belote", value="ㅤ")
-        await self.edits_game_message(embed,
-                                      {"n": card.Card(card.Color.CARREAUX, 11, self.players["n"]),
-                                       "e": card.Card(card.Color.COEUR, 8, self.players["e"]),
-                                       "s": card.Card(card.Color.CARREAUX, 10, self.players["s"])},
-                                      msgs)
 
-        await edits(msgs, components=card.to_buttons([card.Card(card.Color.CARREAUX, 10, None)]))
+        next_player_to_play = "n"
+
+        embed.set_field_at(0,
+                           name="Belote",
+                           value="ㅤ\nㅤ"+self.cards[0].color.value["emoji"]+card.nomber_to_name[self.cards[0].nomber])
+        await edits(msgs, embed=embed)
+
+        p_second = True
+        run = True
+
+        while run:
+            for i in start_player_to_play_list(next_player_to_play):
+                await msgs[i].edit(components=card.to_buttons(self.hand_card[i], [])+[yes_no_button])
+
+                def check(inter):
+                    return inter.message.id == msgs[i].id and self.players[i] == inter.author
+
+                inter = await msgs[i].wait_for_button_click(check)
+                await inter.reply(content="a", type=6)
+
+                await msgs[i].edit(components=card.to_buttons(self.hand_card[i], []))
+                if inter.clicked_button.custom_id == "y":
+                    card.set_atout_color(self.cards[0])
+                    p_second = False
+                    run = False
+                    next_player_to_play = i
+                    break
+
+            if p_second:
+                for i in start_player_to_play_list(next_player_to_play):
+                    await msgs[i].edit(components=card.to_buttons(self.hand_card[i], []) + [deux_button])
+
+                    def check(inter):
+                        return inter.message.id == msgs[i].id and self.players[i] == inter.author
+
+                    inter = await msgs[i].wait_for_button_click(check)
+                    await inter.reply(content="a", type=6)
+
+                    await msgs[i].edit(components=card.to_buttons(self.hand_card[i], []))
+                    if inter.clicked_button.custom_id != "de":
+                        card.set_atout_color({"co": card.Color.COEUR,
+                                              "ca": card.Color.CARREAUX,
+                                              "tr": card.Color.TREFLE,
+                                              "pi": card.Color.PIQUE
+                                              }[inter.clicked_button.custom_id])
+                        next_player_to_play = i
+                        run = False
+                        break
+            if run:
+                next_player_to_play = get_next[next_player_to_play]
+
+        for i in range(8):
+
+
 
     def get_ready_msg(self, embed, i):
         embed.set_field_at(index=0,
@@ -101,32 +210,52 @@ class Game:
                            )
         return embed
 
-    async def edits_game_message(self, embed, val, msgs):
-        for i in self.players.keys():
-            embed.set_field_at(index=0,
-                               name="Belote",
-                               value="ㅤ" +
-                                     "\nㅤㅤㅤㅤ" + (val[pos_to_relative_pos[i]["n"]].color.value["emoji"] +
-                                                 card.nomber_to_name[val[pos_to_relative_pos[i]["n"]].nomber]
-                                                 if pos_to_relative_pos[i]["n"] in val else
-                                                 "ㅤㅤ") +
-                                     "\n\n" + (val[pos_to_relative_pos[i]["o"]].color.value["emoji"] +
-                                               card.nomber_to_name[val[pos_to_relative_pos[i]["o"]].nomber]
-                                               if pos_to_relative_pos[i]["o"] in val else
-                                               "ㅤㅤ") + "\nㅤㅤㅤㅤㅤㅤㅤㅤ" +
-                                     (val[pos_to_relative_pos[i]["e"]].color.value["emoji"] +
-                                      card.nomber_to_name[val[pos_to_relative_pos[i]["e"]].nomber]
-                                      if pos_to_relative_pos[i]["e"] in val else
-                                      "ㅤㅤ") +
-                                     "\n\nㅤㅤㅤㅤ" + (
-                                         val[pos_to_relative_pos[i]["s"]].color.value["emoji"] +
-                                         card.nomber_to_name[val[pos_to_relative_pos[i]["s"]].nomber]
-                                         if pos_to_relative_pos[i]["s"] in val else
-                                         "ㅤㅤ") + "\n"
-                               )
-            await msgs[i].edit(embed=embed)
+    def get_game_message(self, embed, val, i):
+        embed.set_field_at(index=0,
+                           name="Belote",
+                           value="ㅤ" +
+                                 "\nㅤㅤㅤㅤ" + (val[pos_to_relative_pos[i]["n"]].color.value["emoji"] +
+                                             card.nomber_to_name[val[pos_to_relative_pos[i]["n"]].nomber]
+                                             if pos_to_relative_pos[i]["n"] in val else
+                                             "ㅤㅤ") +
+                                 "\n\n" + (val[pos_to_relative_pos[i]["o"]].color.value["emoji"] +
+                                           card.nomber_to_name[val[pos_to_relative_pos[i]["o"]].nomber]
+                                           if pos_to_relative_pos[i]["o"] in val else
+                                           "ㅤㅤ") + "\nㅤㅤㅤㅤㅤㅤㅤㅤ" +
+                                 (val[pos_to_relative_pos[i]["e"]].color.value["emoji"] +
+                                  card.nomber_to_name[val[pos_to_relative_pos[i]["e"]].nomber]
+                                  if pos_to_relative_pos[i]["e"] in val else
+                                  "ㅤㅤ") +
+                                 "\n\nㅤㅤㅤㅤ" + (
+                                     val[pos_to_relative_pos[i]["s"]].color.value["emoji"] +
+                                     card.nomber_to_name[val[pos_to_relative_pos[i]["s"]].nomber]
+                                     if pos_to_relative_pos[i]["s"] in val else
+                                     "ㅤㅤ") + "\n"
+                           )
+        return embed
+
+    def distribue(self, card, player=None):
+        if player is None:
+            for i in self.players:
+                self.hand_card[i] += self.cards[0:card]
+                del self.cards[0:card]
+        else:
+            self.hand_card[player] += self.cards[0:card]
+            del self.cards[0:card]
 
 
 async def edits(msgs, **kwargs):
     for msg in msgs.values():
         await msg.edit(**kwargs)
+
+def start_player_to_play_list(f_p, s=None, l=None):
+    if l is None:
+        l = []
+    if f_p==s:
+        return l
+    else:
+        l.append(f_p)
+        if s is None:
+            return start_player_to_play_list(get_next[f_p], f_p, l)
+        else:
+            return start_player_to_play_list(get_next[f_p], s, l)
