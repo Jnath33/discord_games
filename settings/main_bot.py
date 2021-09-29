cf2cd863109cb5e42edc74377b365b5e52c8f15b
nathan <nathan>
2021-09-29 14:11:23 +0200

    Next version

--- settings/main_bot.py
+++ settings/main_bot.py
@@ -1,72 +1,75 @@
-# Imports
 import json
 import sys
 import random
 
-import discord
 from discord.ext import commands
 
-# Init the bot
+# Init the command count variable
+current_cmd_count = 0
 
-c_id = 0
+# init the variable neded to calculate the bot repartition
 bots_member_serv = {}
 total_bots_member_serv = 0
-
-with open("settings.json", "r") as f:
-    g_settings = json.load(f)
-bot_settings = json.loads(sys.argv[1])
-bot = commands.Bot(command_prefix='!')
-id_max = int(sys.argv[2])
-mode_to_n_of_bot = {"rps": 0,  # Pierre feuille ciseaux
-                    "morp": 1,  # Morpion
-                    "p4": 2,  # Puissance 4
-                    "c_name": 3,  # Code name
-                    "belote": 4,  # Bellote
-                    "presi": 5,  # Président
-                    "u_cover": 6,  # Undercover
-                    "dame": 7,  # Dame
-                    "chess": 8,  # Échec
-                    "ksuv": 9  # Ksuv (jeu histoire)
-                    }
-
 guild_to_bots = {}
 
+# loads the setings and launch the bot
+with open("settings.json", "r") as f:
+    g_settings = json.load(f)
 
+bot_settings = json.loads(sys.argv[1])
+id_max = int(sys.argv[2])  # The maximum id
+
+bot = commands.Bot(command_prefix='!')
+
+# The list of game with the number of bot in the server needed to run the game
+mode_to_n_of_bot = \
+    {
+        "rps": 0,  # Pierre feuille ciseaux,
+        "pend": 0, # Pendu
+        "morp": 1,  # Morpion
+        "p4": 2,  # Puissance 4
+        "c_name": 3,  # Code name
+        "belote": 4,  # Bellote
+        "presi": 5,  # Président
+        "u_cover": 6,  # Undercover
+        "dame": 7,  # Dame
+        "chess": 8,  # Échec
+        "ksuv": 9  # Ksuv (jeu histoire)
+    }
+
+
+# Set's global usage function
 async def game_can_be_start(mode, guild, ctx):
-    if len(guild_to_bots[guild.id]) >= mode_to_n_of_bot[mode]:
-
+    if len(guild_to_bots[guild.id]) >= mode_to_n_of_bot[mode] or g_settings["main_serv_id"] == guild.id:
+        return True
     else:
         await ctx.send("Vous n'avez pas assez de bot !setup")
-
-
-
-for i in range(id_max + 1):
-    bots_member_serv[i] = 0
-
-for s in bot.guilds:
-    for i in range(id_max + 1):
-        bots_member_serv[i] += s.member_count
+    return False
 
 
 def get_r_id(guild):
-    return str(random.randint(0, id_max))
+    return str(random.randint(0, len(guild_to_bots[guild.id])))
 
 
 def send_command(g_name, ctx, args):
-    global c_id
-    t_id, c_id = c_id, c_id + 1
-    with open("bot" + get_r_id(ctx.guild) + "/data/" + str(c_id) + ".json", "x") as f:
+    if game_can_be_start(g_name, ctx.guild, ctx):
+        return
+    global current_cmd_count
+    t_id, current_cmd_count = current_cmd_count, current_cmd_count + 1
+    with open("bot" + get_r_id(ctx.guild) + "/" +
+              "data/" +
+              str(current_cmd_count) + "-cmd.json", "x") as f:
         f.write(json.dumps({"cmd": g_name, "c_id": ctx.channel.id,
                             "args": args},
                            separators=(',', ':')))
 
 
 def register_guild(guild):
-    global guild_to_bot_count
-    guild_to_bot_count[guild.id] = 0
+    global guild_to_bots
+    guild_to_bots[guild.id] = 0
     for it in g_settings["settings"]:
         if guild.get_member(int(it["client_id"])) is not None:
-            guild_to_bot_count[guild.id] += 1
+            guild_to_bots[guild.id] += it
 
 
 async def clear_game_channel():
@@ -78,6 +81,16 @@ async def clear_game_channel():
     print("end of clear")
 
 
+# Set's the variable needed to calculate the bot repartition
+for i in range(id_max + 1):
+    bots_member_serv[i] = 0
+
+for s in bot.guilds:
+    for i in range(id_max + 1):
+        bots_member_serv[i] += s.member_count
+
+
+# bot's function
 @bot.event
 async def on_ready():
     print("Main Bot IS Ready")
@@ -87,6 +100,11 @@ async def on_ready():
 @bot.command(aliases=["p_4", "p4", "power_4", "power4"])
 async def power_four(ctx):
     send_command("p4", ctx, ctx.message.id)
+
+
+@bot.command(aliases=["pend"])
+async def pendu():
+    print("")
 
 
 @bot.command(aliases=["morp"])
@@ -99,10 +117,10 @@ async def bellote(ctx, point: int = 1000):
     send_command("belote", ctx, [point])
 
 
-# create rps cmd with two aliases chifoumi and RPS
 @bot.command(aliases=["chifoumi", "RPS"])
 async def rps(ctx, *args):
     send_command("rps", ctx, [ctx.message.id, *args])
 
 
+# run bot
 bot.run(bot_settings["token"])
